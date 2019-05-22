2618b0cf08dd70ef47792568f010f7a29eccf9dd
kenan666 <1746740633@qq.com>
2019-05-22 15:45:52 +0800

    sitk_Registration12

---
+++ pandas_exercise/SimpleITK_py/sitk_Registration12.py
@@ -0,0 +1,143 @@
+# Data - Retrospective Image Registration Evaluation
+import SimpleITK as sitk
+
+# Utility method that either downloads data from the Girder repository or
+# if already downloaded returns the file name for reading from disk (cached data).
+%run update_path_to_download_script
+from downloaddata import fetch_data as fdata
+
+# Always write output to a separate directory, we don't want to pollute the source directory. 
+import os
+OUTPUT_DIR = 'Output'
+
+# Utility functions
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+from ipywidgets import interact, fixed
+from IPython.display import clear_output
+
+# Callback invoked by the interact IPython method for scrolling through the image stacks of
+# the two images (moving and fixed).
+def display_images(fixed_image_z, moving_image_z, fixed_npa, moving_npa):
+    # Create a figure with two subplots and the specified size.
+    plt.subplots(1,2,figsize=(10,8))
+    
+    # Draw the fixed image in the first subplot.
+    plt.subplot(1,2,1)
+    plt.imshow(fixed_npa[fixed_image_z,:,:],cmap=plt.cm.Greys_r);
+    plt.title('fixed image')
+    plt.axis('off')
+    
+    # Draw the moving image in the second subplot.
+    plt.subplot(1,2,2)
+    plt.imshow(moving_npa[moving_image_z,:,:],cmap=plt.cm.Greys_r);
+    plt.title('moving image')
+    plt.axis('off')
+    
+    plt.show()
+
+# Callback invoked by the IPython interact method for scrolling and modifying the alpha blending
+# of an image stack of two images that occupy the same physical space. 
+def display_images_with_alpha(image_z, alpha, fixed, moving):
+    img = (1.0 - alpha)*fixed[:,:,image_z] + alpha*moving[:,:,image_z] 
+    plt.imshow(sitk.GetArrayViewFromImage(img),cmap=plt.cm.Greys_r);
+    plt.axis('off')
+    plt.show()
+    
+# Callback invoked when the StartEvent happens, sets up our new data.
+def start_plot():
+    global metric_values, multires_iterations
+    
+    metric_values = []
+    multires_iterations = []
+
+# Callback invoked when the EndEvent happens, do cleanup of data and figure.
+def end_plot():
+    global metric_values, multires_iterations
+    
+    del metric_values
+    del multires_iterations
+    # Close figure, we don't want to get a duplicate of the plot latter on.
+    plt.close()
+
+# Callback invoked when the IterationEvent happens, update our data and display new figure.
+def plot_values(registration_method):
+    global metric_values, multires_iterations
+    
+    metric_values.append(registration_method.GetMetricValue())                                       
+    # Clear the output area (wait=True, to reduce flickering), and plot current data
+    clear_output(wait=True)
+    # Plot the similarity metric values
+    plt.plot(metric_values, 'r')
+    plt.plot(multires_iterations, [metric_values[index] for index in multires_iterations], 'b*')
+    plt.xlabel('Iteration Number',fontsize=12)
+    plt.ylabel('Metric Value',fontsize=12)
+    plt.show()
+    
+# Callback invoked when the sitkMultiResolutionIterationEvent happens, update the index into the 
+# metric_values list. 
+def update_multires_iterations():
+    global metric_values, multires_iterations
+    multires_iterations.append(len(metric_values))
+
+
+# Read images
+fixed_image =  sitk.ReadImage(fdata("training_001_ct.mha"), sitk.sitkFloat32)
+moving_image = sitk.ReadImage(fdata("training_001_mr_T1.mha"), sitk.sitkFloat32) 
+
+interact(display_images, fixed_image_z=(0,fixed_image.GetSize()[2]-1), moving_image_z=(0,moving_image.GetSize()[2]-1), fixed_npa = fixed(sitk.GetArrayViewFromImage(fixed_image)), moving_npa=fixed(sitk.GetArrayViewFromImage(moving_image)));
+
+# Initial Alignment
+
+# Use the CenteredTransformInitializer to align the centers of the two volumes and set the center of rotation to the center of the fixed image.
+initial_transform = sitk.CenteredTransformInitializer(fixed_image, 
+                                                      moving_image, 
+                                                      sitk.Euler3DTransform(), 
+                                                      sitk.CenteredTransformInitializerFilter.GEOMETRY)
+
+moving_resampled = sitk.Resample(moving_image, fixed_image, initial_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+
+interact(display_images_with_alpha, image_z=(0,fixed_image.GetSize()[2]-1), alpha=(0.0,1.0,0.05), fixed = fixed(fixed_image), moving=fixed(moving_resampled));
+
+# Registration
+registration_method = sitk.ImageRegistrationMethod()
+
+# Similarity metric settings.
+registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+registration_method.SetMetricSamplingPercentage(0.01)
+
+registration_method.SetInterpolator(sitk.sitkLinear)
+
+# Optimizer settings.
+registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100, convergenceMinimumValue=1e-6, convergenceWindowSize=10)
+registration_method.SetOptimizerScalesFromPhysicalShift()
+
+# Setup for the multi-resolution framework.            
+registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4,2,1])
+registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2,1,0])
+registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+
+# Don't optimize in-place, we would possibly like to run this cell multiple times.
+registration_method.SetInitialTransform(initial_transform, inPlace=False)
+
+# Connect all of the observers so that we can perform plotting during registration.
+registration_method.AddCommand(sitk.sitkStartEvent, start_plot)
+registration_method.AddCommand(sitk.sitkEndEvent, end_plot)
+registration_method.AddCommand(sitk.sitkMultiResolutionIterationEvent, update_multires_iterations) 
+registration_method.AddCommand(sitk.sitkIterationEvent, lambda: plot_values(registration_method))
+
+final_transform = registration_method.Execute(sitk.Cast(fixed_image, sitk.sitkFloat32), 
+                                               sitk.Cast(moving_image, sitk.sitkFloat32))
+
+# Post registration analysis
+print('Final metric value: {0}'.format(registration_method.GetMetricValue()))
+print('Optimizer\'s stopping condition, {0}'.format(registration_method.GetOptimizerStopConditionDescription()))
+
+moving_resampled = sitk.Resample(moving_image, fixed_image, final_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+
+interact(display_images_with_alpha, image_z=(0,fixed_image.GetSize()[2] - 1), alpha=(0.0,1.0,0.05), fixed = fixed(fixed_image), moving=fixed(moving_resampled));、
+
+sitk.WriteImage(moving_resampled, os.path.join(OUTPUT_DIR, 'RIRE_training_001_mr_T1_resampled.mha'))
+sitk.WriteTransform(final_transform, os.path.join(OUTPUT_DIR, 'RIRE_training_001_CT_2_mr_T1.tfm'))
